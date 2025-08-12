146d9eb163484ac1c5e95d3c5ac2a6384e2de185
marialunar <87587062+marialunar@users.noreply.github.com>
2025-08-11 22:16:19 -0300

    Add files via upload

---
+++ tn-poo-clase-01/Clase01.java
@@ -0,0 +1,56 @@
+public class Clase01 {
+    public static void main(String[] args) {
+        /*
+         * Instituto: Centro de Formacion Profesional N°8 - SMATA
+         * Curso: Programacion Orientada a Objetos
+         * Lenguaje principal:Java
+         * Cursada: Lunes, Miercoles, Viernes de 19 a 22.20hs.
+         * Profesor: Francisco Acuña
+         * Email: franciscoacuna.centro8@gmail.com
+         * Repositorio: https://github.com/Francisco-Acuna/2025_2C_POO_TURNO_NOCHE
+         * Softwares necesarios:
+         * - JDK 21 -> 
+         * - Visual Studio Code ->
+         * - MySQL y Workbench ->
+         * - Extension de VSC ->
+         */
+
+        // comentarios en linea
+
+        /*
+         * comentarios
+         * en
+         * bloque
+         */
+
+         /**
+          * Comentarios
+          * del tipo
+          * JavaDoc
+          */
+        
+        // etiquetas en los coemntarios
+
+        // TODO: indica tareas pendientes a implementar o finalizar
+
+        // FIXME: señalan errores o problemas que deben ser corregidos
+
+        //estas palabras no son parte del lenguaje Java como tal, sino convenciones que se usan
+        //dentro de los comentarios. Muchos editores IDEs las reconocen y resaltan automaticamente
+        //para facilitar la colaboracion y el seguimeinto de tareas en el codigo.
+
+        //instalar la extension TODO TREE de Gruntfuggly
+
+        //impresion por consola
+        System.out.println("Hola Mundo");
+        //sout+tab -> atajo para impresion por consola
+        //ctrl+shift+k -> elimina una linea
+
+        //declaracion de variable
+        int variable;//declaracion de variable con tipo de dato e identificador
+        variable = 10; //asignacion de valor a la variable
+        int variable2 = 10; // decalracion y asignacion de valor en linea
+        int variable3 = 10, variable4 =20, variable5 = 30; //declaracion y asignacion multiple en linea
+    
+    }// fin del metodo main
+}//fin de la clase
